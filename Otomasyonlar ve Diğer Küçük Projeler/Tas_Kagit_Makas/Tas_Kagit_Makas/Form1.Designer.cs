85eac3ea2b8dbfe669e73c5c7b7b2f5d684fa569
YCH88 <yrkn88@gmail.com>
2020-07-15 19:16:43 +0300

    Tas Kagıt Makas Eklendi

---
+++ Otomasyonlar ve Diğer Küçük Projeler/Tas_Kagit_Makas/Tas_Kagit_Makas/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Tas_Kagit_Makas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.computer = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Montserrat Alternates", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label.Location = new System.Drawing.Point(314, 35);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(84, 92);
+            this.label.TabIndex = 5;
+            this.label.Text = "0";
+            // 
+            // computer
+            // 
+            this.computer.AutoSize = true;
+            this.computer.Font = new System.Drawing.Font("Montserrat Alternates", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.computer.Location = new System.Drawing.Point(475, 35);
+            this.computer.Name = "computer";
+            this.computer.Size = new System.Drawing.Size(84, 92);
+            this.computer.TabIndex = 6;
+            this.computer.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(131, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Oyuncu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(668, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Bilgisayar";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Tas_Kagit_Makas.Properties.Resources.makas;
+            this.pictureBox5.Location = new System.Drawing.Point(587, 392);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Tas_Kagit_Makas.Properties.Resources.kagit;
+            this.pictureBox4.Location = new System.Drawing.Point(382, 392);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Tas_Kagit_Makas.Properties.Resources.tas;
+            this.pictureBox3.Location = new System.Drawing.Point(183, 392);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(587, 153);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(214, 164);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(69, 153);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(214, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(828, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(41, 28);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(781, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 28);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "↔";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button2_MouseMove);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(881, 572);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.computer);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        public System.Windows.Forms.Label label;
+        public System.Windows.Forms.Label computer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Otomasyonlar ve Diğer Küçük Projeler/Tas_Kagit_Makas/Tas_Kagit_Makas/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tas_Kagit_Makas
+{
+    public partial class Form1 : Form
+    {
+        public static bool sec = true;
+        public static char secim = ' ';
+        public static int puanoyuncu = 0;
+        public static int puanbilgisayar = 0;
+        public static int basili = 0;
+        Point temp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+           
+
+            if (sec)
+            {
+                pictureBox1.Image = pictureBox3.Image;
+                secim = 't';
+                sec = false;
+                hesap(secim);
+            }
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (sec)
+            {
+                pictureBox1.Image = pictureBox4.Image;
+                secim = 'k';
+                sec = false;
+                hesap(secim);
+            }
+
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            if (sec)
+            {
+                pictureBox1.Image = pictureBox5.Image;
+                secim = 'm';
+                sec = false;
+                hesap(secim);
+            }
+        }
+
+        public void hesap(char s)
+        {
+
+            Random rnd = new Random();
+            int sayi = rnd.Next(1, 4);
+            char bilgisayar = ' ';
+            if (sayi == 1)
+            {
+                bilgisayar = 't';
+                pictureBox2.Image = pictureBox3.Image;
+            }
+            else if (sayi == 2)
+            {
+                bilgisayar = 'k';
+                pictureBox2.Image = pictureBox4.Image;
+            }
+            else
+            {
+                bilgisayar = 'm';
+                pictureBox2.Image = pictureBox5.Image;
+            }
+
+            if (s == 't')
+            {
+                if (s == 't' && bilgisayar == 'm')
+                {
+                    MessageBox.Show("Kazandınız");
+                    puanoyuncu++;
+                    label.Text = puanoyuncu.ToString();
+                    sec = true;
+                }
+                else if (s == 't' && bilgisayar == 'k')
+                {
+                    MessageBox.Show("Kaybettiniz");
+                    puanbilgisayar++;
+                    computer.Text = puanbilgisayar.ToString();
+                    sec = true;
+                }
+                else
+                {
+                    MessageBox.Show("Berabere");
+                    sec = true;
+                }
+            }
+            else if (s == 'k')
+            {
+                if (s == 'k' && bilgisayar == 't')
+                {
+                    MessageBox.Show("Kazandınız");
+                    puanoyuncu++;
+                    label.Text = puanoyuncu.ToString();
+                    sec = true;
+                }
+                else if (s == 'k' && bilgisayar == 'm')
+                {
+                    MessageBox.Show("Kaybettiniz");
+                    puanbilgisayar++;
+                    computer.Text = puanbilgisayar.ToString();
+                    sec = true;
+                }
+                else
+                {
+                    MessageBox.Show("Berabere");
+                    sec = true;
+                }
+            }
+
+            if (s == 'm')
+            {
+                if (s == 'm' && bilgisayar == 'k')
+                {
+                    MessageBox.Show("Kazandınız");
+                    puanoyuncu++;
+                    label.Text = puanoyuncu.ToString();
+                    sec = true;
+                }
+                else if (s == 'm' && bilgisayar == 't')
+                {
+                    MessageBox.Show("Kaybettiniz");
+                    puanbilgisayar++;
+                    computer.Text = puanbilgisayar.ToString();
+                    sec = true;
+                }
+                else
+                {
+                    MessageBox.Show("Berabere");
+                    sec = true;
+                }
+            }
+            pictureBox2.Image = null;
+            pictureBox1.Image = null;
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            basili = 1;
+            temp = e.Location;
+        }
+
+        private void button2_MouseUp(object sender, MouseEventArgs e)
+        {
+            basili = 0;
+            this.Update();
+        }
+
+        private void button2_MouseMove(object sender, MouseEventArgs e)
+        {
+           
+            if (basili == 1)
+            {
+                this.Location = new Point(
+                    (this.Location.X - temp.X) + e.X, (this.Location.Y - temp.Y) + e.Y);
+            }
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
